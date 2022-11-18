af5f939ffa2f0701d78865831ff0c03a2a7080ea
Dhruv <tvdhruvraj@outlook.com>
2022-11-18 22:18:43 +0530

    Front End for Applied() and JobsPosted Phase 1

--- client/src/views/components/Customer/AppliedCandidates.js
+++ client/src/views/components/Customer/AppliedCandidates.js
@@ -1,8 +1,74 @@
-import React from "react";
+import * as React from "react";
+import { useHistory } from "react-router-dom";
+import CustomDataGrid from "../../components/CustomDataGrid/CustomDataGrid";
+import Axios from "axios";
 
-const AppliedCandidates = () =>{
+const AppliedCandidates = (props) => {
+  const history = useHistory();
+  console.log("props", props);
 
-    return <div>Welcome AppliedCandidates</div>
-}
+  const [state, setState] = React.useState({ columns: [], rows: [] });
+
+  React.useEffect(() => {
+    const columns = [
+      { field: "id", headerName: "S.No", width: 90 },
+      {
+        field: "jobseekername",
+        headerName: "Name",
+        width: 150,
+        editable: false,
+      },
+      {
+        field: "location",
+        headerName: "Wage Per Day",
+        type: "number",
+        width: 110,
+        editable: false,
+      },
+      {
+        field: "mobileno",
+        headerName: "Mobile Number",
+        width: 150,
+        editable: false,
+      },
+      {
+        field: "email",
+        headerName: "Email Address",
+        width: 150,
+        editable: false,
+      },
+      {
+        field: "yearofxp",
+        headerName: "Year(s) of Experience",
+        width: 110,
+        editable: false,
+      },
+
+      {
+        field: "userid",
+        headerName: "User",
+        width: 110,
+        editable: false,
+      },
+    ];
+
+    async function fetch() {
+      await Axios.get(process.env.REACT_APP_ServerHost + `job_seeker`).then((res) => {
+        if (res.status === 200) {
+          setState({ columns: columns, rows: res.data.response });
+          return res.data.response;
+        }
+        return res;
+      });
+    }
+    fetch();
+  }, []);
+
+  return (
+    <div style={{ width: "65%", margin: "2%" }}>
+      <CustomDataGrid columns={state.columns} rows={state.rows} />
+    </div>
+  );
+};
 
 export default AppliedCandidates;
--- client/src/views/components/JobSeeker/AppliedJobs.js
+++ client/src/views/components/JobSeeker/AppliedJobs.js
@@ -1,8 +1,79 @@
-import React from "react";
+import * as React from "react";
+import { useHistory } from "react-router-dom";
+import CustomDataGrid from "../../components/CustomDataGrid/CustomDataGrid";
+import Axios from "axios";
+import { DateTime } from "luxon";
 
-const AppliedJobs = () =>{
+const AppliedJobs = (props) => {
+  const history = useHistory();
+  console.log("props", props);
 
-    return <div>Welcome AppliedJobs</div>
-}
+  const [state, setState] = React.useState({ columns: [], rows: [] });
+
+  React.useEffect(() => {
+    const columns = [
+      { field: "id", headerName: "S.No", width: 90 },
+      {
+        field: "jobdescription",
+        headerName: "Job Description",
+        width: 150,
+        editable: false,
+      },
+      {
+        field: "wageperday",
+        headerName: "Wage Per Day",
+        type: "number",
+        width: 110,
+        editable: false,
+      },
+      {
+        field: "location",
+        headerName: "Location",
+        width: 150,
+        editable: false,
+      },
+      {
+        field: "fromdate",
+        headerName: "From Date",
+        width: 150,
+        editable: false,
+        valueGetter: (params) =>
+          `${DateTime.fromISO(params.row.fromdate).toFormat("dd-MM-yyyy")}`,
+      },
+      {
+        field: "todate",
+        headerName: "To Date",
+        width: 110,
+        editable: false,
+        valueGetter: (params) =>
+          `${DateTime.fromISO(params.row.todate).toFormat("dd-MM-yyyy")}`,
+      },
+
+      {
+        field: "userid",
+        headerName: "User",
+        width: 110,
+        editable: false,
+      },
+    ];
+
+    async function fetch() {
+      await Axios.get(process.env.REACT_APP_ServerHost + `jobs`).then((res) => {
+        if (res.status === 200) {
+          setState({ columns: columns, rows: res.data.response });
+          return res.data.response;
+        }
+        return res;
+      });
+    }
+    fetch();
+  }, []);
+
+  return (
+    <div style={{ width: "65%", margin: "2%" }}>
+      <CustomDataGrid columns={state.columns} rows={state.rows} />
+    </div>
+  );
+};
 
 export default AppliedJobs;
